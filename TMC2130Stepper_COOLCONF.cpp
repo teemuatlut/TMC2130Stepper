186bc3616b379da9716f38c91a7c4687e33b1534
teemuatLUT <teemu.mantykallio@live.fi>
2016-08-23 20:25:49 +0300

    Add debug messages to functions

--- TMC2130Stepper_COOLCONF.cpp
+++ TMC2130Stepper_COOLCONF.cpp
@@ -12,6 +12,10 @@ void TMC2130Stepper::COOLCONF(uint32_t value) {
 uint8_t TMC2130Stepper::sg_min() {return val_semin;}
 
 void TMC2130Stepper::sg_min(uint8_t value) {
+#ifdef TMC2130DEBUG
+	Serial.print("Set semin: ");
+	Serial.println(value);
+#endif
 	if (value > 15) value = 15;
 	val_semin = value;
 	send2130(WRITE|REG_COOLCONF, &cur_COOLCONF, value, 0xF);
@@ -20,6 +24,10 @@ void TMC2130Stepper::sg_min(uint8_t value) {
 uint8_t TMC2130Stepper::sg_step_width() {return val_seup;}
 
 void TMC2130Stepper::sg_step_width(uint8_t value) {
+#ifdef TMC2130DEBUG
+	Serial.print("Set seup: ");
+	Serial.println(value);
+#endif
 	uint8_t valid[] = {8, 4, 2, 1};
 
 	if (value < valid[3]) value = valid[3]; // Make sure we find a match for low values
@@ -37,6 +45,10 @@ void TMC2130Stepper::sg_step_width(uint8_t value) {
 uint8_t TMC2130Stepper::sg_max() {return val_semax;}
 
 void TMC2130Stepper::sg_max(uint8_t value) {
+#ifdef TMC2130DEBUG
+	Serial.print("Set semax: ");
+	Serial.println(value);
+#endif
 	if (value > 15) value = 15;
 	val_semin = value;
 	send2130(WRITE|REG_COOLCONF, &cur_COOLCONF, (uint32_t)(uint32_t)value << 8, 0xF00);
@@ -45,6 +57,10 @@ void TMC2130Stepper::sg_max(uint8_t value) {
 uint8_t TMC2130Stepper::sg_current_decrease() {return val_sedn;}
 
 void TMC2130Stepper::sg_current_decrease(uint8_t value) {
+#ifdef TMC2130DEBUG
+	Serial.print("Set sedn: ");
+	Serial.println(value);
+#endif
 	uint8_t valid[] = {32, 8, 2, 1};
 
 	if (value < valid[3]) value = valid[3]; // Make sure we find a match for low values
@@ -62,6 +78,10 @@ void TMC2130Stepper::sg_current_decrease(uint8_t value) {
 uint8_t TMC2130Stepper::smart_min_current() {return val_seimin;}
 
 void TMC2130Stepper::smart_min_current(uint8_t value) {
+#ifdef TMC2130DEBUG
+	Serial.print("Set seimin: ");
+	Serial.println(value);
+#endif
 	if (value > 1) value = 1;
 	val_seimin = value;
 	send2130(WRITE|REG_COOLCONF, &cur_COOLCONF, (uint32_t)value << 15, 0x8000);
@@ -70,6 +90,10 @@ void TMC2130Stepper::smart_min_current(uint8_t value) {
 int8_t TMC2130Stepper::sg_stall_value() {return val_sgt;}
 
 void TMC2130Stepper::sg_stall_value(int8_t value) {
+#ifdef TMC2130DEBUG
+	Serial.print("Set sgt: ");
+	Serial.println(value);
+#endif
 	if (value < -64) value = -64;
 	else if (value > 63) value = 63;
 	val_sgt = value;
@@ -79,6 +103,10 @@ void TMC2130Stepper::sg_stall_value(int8_t value) {
 uint8_t TMC2130Stepper::sg_filter() {return val_sfilt;}
 
 void TMC2130Stepper::sg_filter(uint8_t value) {
+#ifdef TMC2130DEBUG
+	Serial.print("Set sfilt: ");
+	Serial.println(value);
+#endif
 	if (value > 1) value = 1;
 	val_sfilt = value;
 	send2130(WRITE|REG_COOLCONF, &cur_COOLCONF, (uint32_t)value << 24, (uint32_t)0b1 << 24);
