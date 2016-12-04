f25ba8c88fb4a9078ce439e097e0695fb6b244d3
teemuatLUT <teemu.mantykallio@live.fi>
2016-12-05 01:26:26 +0200

    Demo stealthChop for maximum effort

--- examples/TMC2130_Simple/TMC2130_Simple.ino
+++ examples/TMC2130_Simple/TMC2130_Simple.ino
@@ -3,7 +3,7 @@
 #define STEP_PIN  54  //			18			54	//step
 #define CS_PIN    40  //			17			64	//chip select
 
-bool dir = false;
+bool dir = 0;
 
 
 #include <TMC2130Stepper.h>
@@ -12,7 +12,8 @@ TMC2130Stepper TMC2130 = TMC2130Stepper(EN_PIN, DIR_PIN, STEP_PIN, CS_PIN);
 void setup() {
 	Serial.begin(9600);
 	TMC2130.begin();
-	TMC2130.SilentStepStick2130(1000);
+	TMC2130.SilentStepStick2130(600);
+	TMC2130.stealthChop(1);
 	
 	digitalWrite(EN_PIN, LOW);
 }
@@ -24,17 +25,17 @@ void loop() {
 	delayMicroseconds(10);
 	uint32_t ms = millis();
 	static uint32_t last_time = 0;
-	if ((ms - last_time) > 4000) {
+	if ((ms - last_time) > 2000) {
 		if (dir) {
-			Serial.println("Dir -> 1");
-			TMC2130.GCONF(0b10001);
-			Serial.println(TMC2130.GCONF(), BIN);
-			dir = false;
-		} else {
 			Serial.println("Dir -> 0");
-			TMC2130.GCONF(0b1);
+			TMC2130.shaft_dir(0);
 			Serial.println(TMC2130.GCONF(), BIN);
-			dir = true;
+			dir = 0;
+		} else {
+			Serial.println("Dir -> 1");
+			TMC2130.shaft_dir(1);
+			Serial.println(TMC2130.GCONF(), BIN);
+			dir = 1;
 		}
 		last_time = ms;
 	}
