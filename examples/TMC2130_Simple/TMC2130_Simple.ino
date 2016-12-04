4a9dd6f55983993e986f15b94bbfb96de6c04dad
teemuatLUT <teemu.mantykallio@live.fi>
2016-12-05 00:54:21 +0200

    Pin renames

--- examples/TMC2130_Simple/TMC2130_Simple.ino
+++ examples/TMC2130_Simple/TMC2130_Simple.ino
@@ -1,27 +1,26 @@
 #define EN_PIN    38  // Nano v3:	16 Mega:	38	//enable (CFG6)
 #define DIR_PIN   55  //			19			55	//direction
 #define STEP_PIN  54  //			18			54	//step
-#define CS_PIN    64  //			17			64	//chip select
+#define CS_PIN    40  //			17			64	//chip select
 
 bool dir = false;
 
 
 #include <TMC2130Stepper.h>
-TMC2130Stepper TMC2130 = TMC2130Stepper(pinEN, pinDIR, pinStep, pinCS);
+TMC2130Stepper TMC2130 = TMC2130Stepper(EN_PIN, DIR_PIN, STEP_PIN, CS_PIN);
 
 void setup() {
 	Serial.begin(9600);
 	TMC2130.begin();
 	TMC2130.SilentStepStick2130(1000);
 	
-	digitalWrite(pinEN, LOW);
-	Serial.println(TMC2130.external_ref());
+	digitalWrite(EN_PIN, LOW);
 }
 
 void loop() {
-	digitalWrite(pinStep, HIGH);
+	digitalWrite(STEP_PIN, HIGH);
 	delayMicroseconds(10);
-	digitalWrite(pinStep, LOW);
+	digitalWrite(STEP_PIN, LOW);
 	delayMicroseconds(10);
 	uint32_t ms = millis();
 	static uint32_t last_time = 0;
