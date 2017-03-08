4f291a3d1991cf32c5644ed02373317727a589ef
teemuatlut <teemuatlut@github.com>
2017-03-08 17:12:33 +0200

    Improve the simple example

--- examples/TMC2130_Simple/TMC2130_Simple.ino
+++ examples/TMC2130_Simple/TMC2130_Simple.ino
@@ -1,10 +1,13 @@
+/*
+Initializes the library and turns the motor in alternating directions.
+*/
+
 #define EN_PIN    38  // Nano v3:	16 Mega:	38	//enable (CFG6)
 #define DIR_PIN   55  //			19			55	//direction
 #define STEP_PIN  54  //			18			54	//step
 #define CS_PIN    40  //			17			64	//chip select
 
-bool dir = 0;
-
+bool dir = true;
 
 #include <TMC2130Stepper.h>
 TMC2130Stepper TMC2130 = TMC2130Stepper(EN_PIN, DIR_PIN, STEP_PIN, CS_PIN);
@@ -12,7 +15,7 @@ TMC2130Stepper TMC2130 = TMC2130Stepper(EN_PIN, DIR_PIN, STEP_PIN, CS_PIN);
 void setup() {
 	Serial.begin(9600);
 	TMC2130.begin(); // Initiate pins and registeries
-	TMC2130.SilentStepStick2130(600); // Same as TMC2130.setCurrent(600, 0.11, 0.5);
+	TMC2130.SilentStepStick2130(600); // Set stepper current to 600mA. The command is the same as command TMC2130.setCurrent(600, 0.11, 0.5);
 	TMC2130.stealthChop(1); // Enable extremely quiet stepping
 	
 	digitalWrite(EN_PIN, LOW);
@@ -29,14 +32,12 @@ void loop() {
 		if (dir) {
 			Serial.println("Dir -> 0");
 			TMC2130.shaft_dir(0);
-			Serial.println(TMC2130.GCONF(), BIN);
-			dir = 0;
 		} else {
 			Serial.println("Dir -> 1");
 			TMC2130.shaft_dir(1);
-			Serial.println(TMC2130.GCONF(), BIN);
-			dir = 1;
 		}
+		dir = !dir;
+		Serial.println(TMC2130.GCONF(), BIN);
 		last_time = ms;
 	}
 }
